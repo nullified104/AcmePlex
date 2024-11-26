f9c1ea2c2dc24764c9eb6e8b7e9391d4e1434689
nullified104 <70874090+nullified104@users.noreply.github.com>
2024-11-26 11:26:20 -0700

    Set up front-end interface with navbar and movies list

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main-container">
+    <NavigationBar/>
+    <MovieList/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavigationBar from './components/NavigationBar.vue';
+import MovieList from './components/MovieList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavigationBar,
+    MovieList,
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@468&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ frontend/src/components/MovieItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="column mb-4">
+        <div class="card-group">
+            <div class="card" style="width: 18rem;">
+                <img src="@/assets/movie.jpg" class="card-img-top img-responsive" alt="Movie">
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Title: {{ title }}</li>
+                        <li class="list-group-item">Genre: {{ genre }}</li>
+                        <li class="list-group-item">Duration: {{ duration }}</li>
+                    </ul>
+                    <a href="" class="btn btn-outline-dark">Check Showtimes</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'MovieItem',
+    props: {
+        id: Number,
+        title: String,
+        genre: String,
+        duration: Number
+    },
+    data() {
+        return {
+            movie: ''
+        }
+    },
+    methods: {
+    },
+    created() {
+    }
+}
+</script>
